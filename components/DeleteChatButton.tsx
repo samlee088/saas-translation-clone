cafc4aaece159d09104640c9c08762e8e150f87d
samlee088 <108655429+samlee088@users.noreply.github.com>
2024-03-15 20:53:15 -0700

    delete button

--- components/DeleteChatButton.tsx
+++ components/DeleteChatButton.tsx
@@ -1,7 +1,61 @@
-import React from "react";
+import { useSession } from "next-auth/react";
+import React, { useState } from "react";
+import { useToast } from "./ui/use-toast";
+import { useRouter } from "next/navigation";
+import useAdminId from "@/hooks/useAdminId";
+import {
+  Dialog,
+  DialogContent,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./ui/dialog";
+import { Button } from "./ui/button";
+import { DialogDescription } from "@radix-ui/react-dialog";
 
 function DeleteChatButton({ chatId }: { chatId: string }) {
-  return <div></div>;
+  const { data: session } = useSession();
+  const [open, setOpen] = useState(false);
+  const { toast } = useToast();
+  const router = useRouter();
+  const adminId = useAdminId({ chatId });
+
+  const handleDelete = async () => {
+    toast({
+      title: "Deleting chat",
+      description: "Please wait while we delete the chat...",
+    });
+
+    console.log("Deleting :: ", chatId);
+  };
+
+  return (
+    session?.user.id === adminId && (
+      <Dialog open={open} onOpenChange={setOpen}>
+        <DialogTrigger asChild>
+          <Button variant="destructive">DeleteChat</Button>
+        </DialogTrigger>
+        <DialogContent className="sm:max-w-md">
+          <DialogHeader>
+            <DialogTitle>Are you sure?</DialogTitle>
+            <DialogDescription>
+              This will delete the chat for all users.
+            </DialogDescription>
+          </DialogHeader>
+
+          <div className="grid grid=cols-2 space-x-2>">
+            <Button variant={"destructive"} onClick={handleDelete}>
+              Delete
+            </Button>
+
+            <Button variant={"outline"} onClick={() => setOpen(false)}>
+              Cancel
+            </Button>
+          </div>
+        </DialogContent>
+      </Dialog>
+    )
+  );
 }
 
 export default DeleteChatButton;
